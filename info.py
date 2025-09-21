ad4dd775787be83920da7c40782868bc05c2e4ff
Asmahan <asmahan.alexander@gmail.com>
2025-09-22 02:27:49 +0300

    info.py

---
+++ info.py
@@ -0,0 +1,3 @@
+
+print("Hello, This is Asmahan Iskandar")
+print("I want to learn coding because it feels like a gateway to new possibilities. I’m curious to explore what I’m capable of and see where it might lead me. Even if it doesn’t become my main path, learning something new is always exciting, and every skill I gain is a valuable asset.")
